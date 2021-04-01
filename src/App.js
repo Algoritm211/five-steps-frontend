21a69bb2b8b8c54b13f4c83fd94635e5fd493536
alexey_horbunov <algoritm211@gmail.com>
2021-04-01 18:49:54 +0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from "react";
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {getIsAuth} from "./store/auth-reducer/auth-selector";
+import {isAppReady} from "./store/app-reducer/app-selector";
+import {Switch, Route, Redirect} from 'react-router-dom'
 
 function App() {
+
+  const dispatch = useDispatch()
+  const isAuth = useSelector(getIsAuth)
+  const isReady = useSelector(isAppReady)
+
   return (
     <div className="App">
-      Hello, five-steps
+      {!isAuth
+        ?
+        <Switch>
+          <Route path={'/registration'} component={() => <div>Registration</div>}  />
+          <Route path={'/login'} component={() => <div>Login</div>} />
+          <Redirect to={'/login'} />
+        </Switch>
+        : <Switch>
+          <Route path={'/'} exact component={() => <div>Main page</div>} />
+          <Route path={'/profile'} component={() => <div>Profile</div>} />
+          <Redirect to={'/'} />
+        </Switch>
+      }
+
     </div>
   );
 }
