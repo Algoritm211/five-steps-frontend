50d138fa7ca250a3e70991c1d8b058cbac1012ff
Tim-Fedor <31453165+Tim-Fedor@users.noreply.github.com>
2021-04-17 16:56:36 +0300

    1. CourseCard update (fix paddings/margins)

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -7,29 +7,29 @@ const CourseCard = (props) => {
 
 	return (
 		<div className="container card ">
-			<div className="row mb-3">
+			<div className="row mb-3 ms-0 me-0">
 				<div className={`col-5 text-center rounded card-tag`}>
 					<p className="text-center m-auto card-tag-text">{category}</p>
 				</div>
 			</div>
-			<div className="row mb-5">
+			<div className="row mb-5 ms-0 me-0">
 				<div className="col-12 p-0">
 					<h2 className="text-left card-title">{title}</h2>
 				</div>
 			</div>
-			<div className="row position-absolute" style={{bottom: '40%', right: '5%'}}>
+			<div className="row position-absolute ms-0 me-0" style={{bottom: '40%', right: '5%'}}>
 				<div className="d-flex flex-row-reverse col-12 align-items-end">
 					<span className="like-text align-text-bottom">{likesCount}</span>
 					<img src="https://pngimg.com/uploads/like/like_PNG39.png" className="" alt="" width="11%" />
 				</div>
 			</div>
-			<div className="row">
+			<div className="row ms-0 me-0">
 				<div className="col-12 p-0">
 					<hr />
 				</div>
 			</div>
-			<div className="row">
-				<div className="col-2">
+			<div className="row ms-0 me-0">
+				<div className="col-2 pe-0 ps-0">
 					<img src={authorAvatar} className="img-fluid" alt="" />
 				</div>
 				<div className="col-10">
