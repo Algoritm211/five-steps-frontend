303ae40653f242d481181dae60e773bccba6eaf4
Erveftick <edwardkvashin@gmail.com>
2021-04-17 19:47:06 +0300

    Fixed adaptive card styles

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -12,13 +12,13 @@ const CourseCard = (props) => {
 					<p className="text-center m-auto card-tag-text">{category}</p>
 				</div>
 			</div>
-			<div className="row mb-5 ms-0 me-0">
+			<div className="row mb-0 ms-0 me-0">
 				<div className="col-12 p-0">
 					<h2 className="text-left card-title">{title}</h2>
 				</div>
 			</div>
-			<div className="row position-absolute ms-0 me-0" style={{bottom: '40%', right: '5%'}}>
-				<div className="d-flex flex-row-reverse col-12 align-items-end">
+			<div className="row ms-0 me-0" style={{bottom: '40%', right: '5%'}}>
+				<div className="container d-flex flex-row-reverse col-12 align-items-end">
 					<span className="like-text align-text-bottom">{likesCount}</span>
 					<img src="https://pngimg.com/uploads/like/like_PNG39.png" className="" alt="" width="11%" />
 				</div>
