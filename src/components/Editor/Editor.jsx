50e9c67784ed10505a5cdced0909ac56a9e61dc0
alexey_horbunov <algoritm211@gmail.com>
2021-04-26 22:29:18 +0300

    Resolve bug with starting project

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -33,71 +33,72 @@ const CourseEditor = () => {
 
 	const onSaveLesson = () => {
 		if (!currentLesson) {
-			dispatch(createLesson({body: lessonBody, homeWork: 'Editor', courseId: course._id}))
+			dispatch(createLesson({ body: lessonBody, homeWork: 'Editor', courseId: course._id }))
 		}
 
-	}
 
-	const lessonBlock = [1, 2, 3, 4, 5].map((lNumber) => {
+		const lessonBlock = [1, 2, 3, 4, 5].map((lNumber) => {
+			return (
+				<React.Fragment key={lNumber}>
+					<div className={`container ${lNumber === lessonNumber ? 'editor-select-active' : 'editor-select'}`}>
+						<NavLink to='#' onClick={() => setLessonNumber(lNumber)}>
+							<p className='editor-step'>Крок {lNumber}</p>
+						</NavLink>
+					</div>
+					<div className='container editor-task'>
+						<NavLink to='#'>
+							<p className='editor-step' style={{ marginLeft: '20px' }}>Завдання</p>
+						</NavLink>
+					</div>
+				</React.Fragment>
+			)
+		})
+
 		return (
-			<React.Fragment key={lNumber} >
-				<div className={`container ${lNumber === lessonNumber ? 'editor-select-active' : 'editor-select'}`}>
-					<NavLink to='#' onClick={() => setLessonNumber(lNumber)}>
-						<p className='editor-step'>Крок {lNumber}</p>
-					</NavLink>
-				</div>
-				<div className='container editor-task'>
-					<NavLink to='#'>
-						<p className='editor-step' style={{ marginLeft: '20px' }}>Завдання</p>
-					</NavLink>
-				</div>
-			</React.Fragment>
-		)
-	})
-
-	return (
-		<MainLayout>
-			<div className='row m-0'>
-				<div className='col-12 col-md-9 d-flex mb-3'>
-					<Editor
-						apiKey={'j2rcg8qaqco0x9y81b1jn5dc0ze3phyfbapmnra5q59deqml'}
-						value={lessonBody}
-						initialValue={currentLesson?.body || ''}
-						init={{
-							height: 500,
-							width: '100%',
-							selector: 'textarea',
-							plugins: [
-								'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-								'searchreplace visualblocks code fullscreen',
-								'insertdatetime media table paste code help wordcount',
-							],
-							toolbar:
-								`undo redo | formatselect | bold italic backcolor | \
+			<MainLayout>
+				<div className='row m-0'>
+					<div className='col-12 col-md-9 d-flex mb-3'>
+						<Editor
+							apiKey={'j2rcg8qaqco0x9y81b1jn5dc0ze3phyfbapmnra5q59deqml'}
+							value={lessonBody}
+							initialValue={currentLesson?.body || ''}
+							init={{
+								height: 500,
+								width: '100%',
+								selector: 'textarea',
+								plugins: [
+									'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+									'searchreplace visualblocks code fullscreen',
+									'insertdatetime media table paste code help wordcount',
+								],
+								toolbar:
+									`undo redo | formatselect | bold italic backcolor | \
 						alignleft aligncenter alignright alignjustify | \
 						bullist numlist outdent indent | removeformat | help`,
-						}}
-						onEditorChange={handleEditorChange}
-					/>
-				</div>
-				<div className='col-12 col-md-3 d-block pe-0'>
-					<div className='container'>
-						<h1 className='editor-title mt-3'>Професія</h1>
-						<h3 className='editor-subtitle mb-3'>{course.title}</h3>
+							}}
+							onEditorChange={handleEditorChange}
+						/>
 					</div>
+					<div className='col-12 col-md-3 d-block pe-0'>
+						<div className='container'>
+							<h1 className='editor-title mt-3'>Професія</h1>
+							<h3 className='editor-subtitle mb-3'>{course.title}</h3>
+						</div>
 
-					<div className='editor-menu'>
-						{lessonBlock}
-					</div>
-					<div className='container d-flex justify-content-center mt-5'>
-						<button className='preview-button mb-3' onClick={onSaveLesson}>
-							Зберегти
-						</button>
+						<div className='editor-menu'>
+							{lessonBlock}
+						</div>
+						<div className='container d-flex justify-content-center mt-5'>
+							<button className='preview-button mb-3' onClick={onSaveLesson}>
+								Зберегти
+							</button>
+						</div>
 					</div>
 				</div>
-			</div>
-		</MainLayout>
-	)
+			</MainLayout>
+		)
+	}
 }
+
 
 export default CourseEditor
