7eaefeb97cf1e81bc57a9ac558f042377116123e
alexey_horbunov <algoritm211@gmail.com>
2021-04-27 00:12:14 +0300

    Solve bug with adding courses

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -17,6 +17,7 @@ const CourseEditor = () => {
 	const [lessonBody, setLessonBody] = useState('')
 
 	useEffect(() => {
+		setLessonBody('')
 		if (course?._id) {
 			dispatch(loadLesson(course?._id, lessonNumber))
 		}
@@ -27,7 +28,7 @@ const CourseEditor = () => {
 	}
 
 	const handleEditorChange = (content, editor) => {
-		console.log('Content was updated:', content)
+		// console.log('Content was updated:', content)
 		setLessonBody(content)
 	}
 
@@ -35,69 +36,69 @@ const CourseEditor = () => {
 		if (!currentLesson) {
 			dispatch(createLesson({ body: lessonBody, homeWork: 'Editor', courseId: course._id }))
 		}
+	}
 
 
-		const lessonBlock = [1, 2, 3, 4, 5].map((lNumber) => {
-			return (
-				<React.Fragment key={lNumber}>
-					<div className={`container ${lNumber === lessonNumber ? 'editor-select-active' : 'editor-select'}`}>
-						<NavLink to='#' onClick={() => setLessonNumber(lNumber)}>
-							<p className='editor-step'>Крок {lNumber}</p>
-						</NavLink>
-					</div>
-					<div className='container editor-task'>
-						<NavLink to='#'>
-							<p className='editor-step' style={{ marginLeft: '20px' }}>Завдання</p>
-						</NavLink>
-					</div>
-				</React.Fragment>
-			)
-		})
-
+	const lessonBlock = [1, 2, 3, 4, 5].map((lNumber) => {
 		return (
-			<MainLayout>
-				<div className='row m-0'>
-					<div className='col-12 col-md-9 d-flex mb-3'>
-						<Editor
-							apiKey={'j2rcg8qaqco0x9y81b1jn5dc0ze3phyfbapmnra5q59deqml'}
-							value={lessonBody}
-							initialValue={currentLesson?.body || ''}
-							init={{
-								height: 500,
-								width: '100%',
-								selector: 'textarea',
-								plugins: [
-									'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-									'searchreplace visualblocks code fullscreen',
-									'insertdatetime media table paste code help wordcount',
-								],
-								toolbar:
-									`undo redo | formatselect | bold italic backcolor | \
+			<React.Fragment key={lNumber}>
+				<div className={`container ${lNumber === lessonNumber ? 'editor-select-active' : 'editor-select'}`}>
+					<NavLink to='#' onClick={() => setLessonNumber(lNumber)}>
+						<p className='editor-step'>Крок {lNumber}</p>
+					</NavLink>
+				</div>
+				<div className='container editor-task'>
+					<NavLink to='#'>
+						<p className='editor-step' style={{ marginLeft: '20px' }}>Завдання</p>
+					</NavLink>
+				</div>
+			</React.Fragment>
+		)
+	})
+
+	return (
+		<MainLayout>
+			<div className='row m-0'>
+				<div className='col-12 col-md-9 d-flex mb-3'>
+					<Editor
+						apiKey={'j2rcg8qaqco0x9y81b1jn5dc0ze3phyfbapmnra5q59deqml'}
+						value={lessonBody}
+						initialValue={currentLesson?.body || ''}
+						init={{
+							height: 500,
+							width: '100%',
+							selector: 'textarea',
+							plugins: [
+								'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+								'searchreplace visualblocks code fullscreen',
+								'insertdatetime media table paste code help wordcount',
+							],
+							toolbar:
+								`undo redo | formatselect | bold italic backcolor | \
 						alignleft aligncenter alignright alignjustify | \
 						bullist numlist outdent indent | removeformat | help`,
-							}}
-							onEditorChange={handleEditorChange}
-						/>
+						}}
+						onEditorChange={handleEditorChange}
+					/>
+				</div>
+				<div className='col-12 col-md-3 d-block pe-0'>
+					<div className='container'>
+						<h1 className='editor-title mt-3'>Професія</h1>
+						<h3 className='editor-subtitle mb-3'>{course.title}</h3>
 					</div>
-					<div className='col-12 col-md-3 d-block pe-0'>
-						<div className='container'>
-							<h1 className='editor-title mt-3'>Професія</h1>
-							<h3 className='editor-subtitle mb-3'>{course.title}</h3>
-						</div>
 
-						<div className='editor-menu'>
-							{lessonBlock}
-						</div>
-						<div className='container d-flex justify-content-center mt-5'>
-							<button className='preview-button mb-3' onClick={onSaveLesson}>
-								Зберегти
-							</button>
-						</div>
+					<div className='editor-menu'>
+						{lessonBlock}
+					</div>
+					<div className='container d-flex justify-content-center mt-5'>
+						<button className='preview-button mb-3' onClick={onSaveLesson}>
+							Зберегти
+						</button>
 					</div>
 				</div>
-			</MainLayout>
-		)
-	}
+			</div>
+		</MainLayout>
+	)
 }
 
 
